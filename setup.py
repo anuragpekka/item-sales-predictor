c37c631f1d2ffabbe06f0bcf66f7207567e241e9
anuragpekka <anuragpekka@gmail.com>
2022-08-28 02:07:24 +0530

    Removed -e from setup.py

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ def get_requirements_list()->List[str]: #List->[str]: Function returns list of s
     with open(REQUIREMENTS_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
         requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
-        #if HYPHEN_E_DOT in requirement_list:
-        #    requirement_list.remove(HYPHEN_E_DOT)
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
         return requirement_list
         
 setup(
